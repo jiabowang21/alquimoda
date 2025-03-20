83deb6f8a33ee602453d35e2696a30673fa893f3
jiabo wang <jiabowang59@gmail.com>
2025-03-20 17:50:58 +0100

    init

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AuthForm from "../components/AuthForm.vue";
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <AuthForm />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
